d02c43f974c3177fee13b635c0ac89ee2e28fe31
Jomkie <937574740@qq2.com>
2023-02-24 10:54:21 +0800

    增加内容

--- single/ConstTest2.cpp
+++ single/ConstTest2.cpp
@@ -14,6 +14,7 @@ class ConstObj {
         const int constData = 3; // 纯常量可以在类内部初始化
         const int constData2;
         ConstObj() : constData2(3000) {} // 纯常量初始化除了一开始声明初始值，也可以按如此形式初始化
+        ConstObj(int data) : constData2(data) { cout << "有参构造方法" << endl; }
 };
 
 // 类的静态成员不能在类中声明时初始化，可以在外部初始化
@@ -21,6 +22,7 @@ int ConstObj::staticData2 = 100;
 
 int main() {
     ConstObj c;
+    ConstObj c2(30);
     cout << c.constData2 << endl;
     cout << c.staticConstData << endl;
     return 0;
