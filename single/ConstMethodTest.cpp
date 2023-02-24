885509029178a165122751f9203a512f0e0e92ec
Jomkie <937574740@qq2.com>
2023-02-24 15:46:16 +0800

    调整内容

--- single/ConstMethodTest.cpp
+++ single/ConstMethodTest.cpp
@@ -8,17 +8,17 @@ using namespace std;
 class Sample {
     public:
         Sample() {}
-        void getValue() const;
         void getValue();
+        void getValue() const;
         void priValue();
-        void priVcon() const;
+        void priValue() const;
         void constMethod() const;
 };
 
 void Sample::getValue() { cout << "非常量函数 getValue" << endl; }
 void Sample::getValue() const { cout << "常量函数 getValue" << endl; }
 void Sample::priValue() { cout << "非常量函数 priValue" << endl; }
-void Sample::priVcon() const { cout << "常量函数 priValue" << endl; }
+void Sample::priValue() const { cout << "常量函数 priValue" << endl; }
 void Sample::constMethod() const { cout << "这是一个 const 函数" << endl; }
 
 int main() 
@@ -27,14 +27,14 @@ int main()
     Sample o;
 
     cono.getValue();
-    //cono.priValue(); 常量对象不能访问非常量方法
-    cono.priVcon();
+    //cono.priValue(); 常量对象不能访问非常量方法，编译会报错
+    cono.priValue();
 
     cout << "-----------------------------" << endl;
 
     o.getValue();
     o.priValue();
-    o.priVcon();
+    o.constMethod(); // 非常量对象可以调用常量方法
 
     return 0;
 }
